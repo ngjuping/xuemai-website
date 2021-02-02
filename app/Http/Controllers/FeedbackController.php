8cf092f3924fd95181dabdd83e8368fdf6361163
ngjuping <ngjuping2018@163.com>
2021-02-03 00:06:22 +0800

    test laravel auto rebuilds backend

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -21,7 +21,7 @@ class FeedbackController extends Controller
             $newFeedback->issue = $request->issue;
             $newFeedback->importance = $request->importance;
             $newFeedback->type = $request->type;
-            $newFeedback->status = 1;
+            $newFeedback->status = 2;
             $newFeedback->remark = $request->remark;
             $newFeedback->save();
 
