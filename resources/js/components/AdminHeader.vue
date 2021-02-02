54f715d915acff0eb16d310e7bb7bfc3b2c6da9d
ngjuping <ngjuping2018@163.com>
2021-02-03 00:04:35 +0800

    Fix admin header bug

--- resources/js/components/AdminHeader.vue
+++ resources/js/components/AdminHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow">
-        <a class="navbar-brand" href="/dashboard">学脉后台</a>
+        <a class="navbar-brand" href="/">学脉后台</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="/dashboard">主页 <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="/">主页 <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="text-white nav-item">
                     <a class="nav-link" href="/">用户视图</a>
